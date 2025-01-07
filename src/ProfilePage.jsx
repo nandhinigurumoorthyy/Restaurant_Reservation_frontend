a7f4b41f999883053e565873c6f73e558a63d51f
nandhinigurumoorthyy <nandhinigurumoorthy2003@gmail.com>
2025-01-08 02:02:50 +0530

    commit profilepage

--- src/ProfilePage.jsx
+++ src/ProfilePage.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const ProfilePage = () => {
   const username = localStorage.getItem("username");
   const email = localStorage.getItem("email");
+  const navigate = useNavigate();
 
   const [bookings, setBookings] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -34,6 +36,21 @@ const ProfilePage = () => {
     }
   }, [email]);
 
+  //handle logout
+  const handleLogout = async () => {
+    try {
+      // Example: Clear session or localStorage (if required)
+      localStorage.removeItem("username");
+      localStorage.removeItem("email");
+
+      // Redirect after successful logout
+      navigate("/");
+    } catch (error) {
+      console.error("Logout failed:", error);
+      // You can display an error message to the user if needed
+    }
+  };
+
   // Handle delete booking
   const handleDelete = async (bookingId) => {
     if (window.confirm("Are you sure you want to delete this booking?")) {
@@ -59,9 +76,10 @@ const ProfilePage = () => {
   const handleEditSubmit = async (e) => {
     e.preventDefault();
     try {
+      const { partySize, date, partyTime } = editingBooking;
       const response = await axios.put(
         `http://localhost:10000/api/bookings/${editingBooking._id}`,
-        editingBooking
+        { partySize, date, partyTime }
       );
       setBookings((prevBookings) =>
         prevBookings.map((booking) =>
@@ -80,12 +98,19 @@ const ProfilePage = () => {
     <div>
       <Navbar />
       <div className="min-h-full mx-4 px-4 pt-3 font-serif">
+        <figure className="flex items-center justify-center pt-2">
+          <img
+            className="w-80"
+            src="https://www.icegif.com/wp-content/uploads/2023/07/icegif-500.gif"
+            alt=""
+          />
+        </figure>
         <div className="flex gap-4 text-xl">
-          <span className="font-semibold">User Name</span>
+          <span className="font-semibold">User Name - </span>
           <span>{username}</span>
         </div>
         <div className="flex gap-4 text-xl">
-          <span className="font-semibold">User Email</span>
+          <span className="font-semibold">User Email - </span>
           <span>{email}</span>
         </div>
 
@@ -113,14 +138,14 @@ const ProfilePage = () => {
                     <button
                       type="button"
                       onClick={() => handleEdit(booking)}
-                      className="text-blue-700 hover:text-blue-800 border-2 border-blue-700 rounded-lg px-2 py-1"
+                      className="text-red-700 hover:text-red-800 border-2 border-red-700 rounded-lg px-3 py-1 hover:border-gray-600 hover:bg-red-700 hover:text-white"
                     >
                       Edit
                     </button>
                     <button
                       type="button"
                       onClick={() => handleDelete(booking._id)}
-                      className="text-red-700 hover:text-red-800 border-red-700 border-2 rounded-lg px-2 py-1"
+                      className="text-red-700 hover:text-red-800 border-red-700 border-2 rounded-lg px-2 py-1 hover:border-gray-600 hover:bg-red-700 hover:text-white"
                     >
                       Delete
                     </button>
@@ -137,24 +162,15 @@ const ProfilePage = () => {
           <div className="EditBooking mt-10">
             <h2 className="text-2xl font-semibold mb-4">Edit Booking:</h2>
             <form onSubmit={handleEditSubmit}>
-              <div>
-                <label>
-                  Restaurant Name:
-                  <input
-                    type="text"
-                    value={editingBooking.restaurantName}
-                    onChange={(e) =>
-                      setEditingBooking((prev) => ({
-                        ...prev,
-                        restaurantName: e.target.value,
-                      }))
-                    }
-                  />
+              <div className="pb-2">
+                <label className="text-xl">
+                  <span className="font-semibold">Restaurant Name: </span>
+                  <span className="ml-2">{editingBooking.restaurantName}</span>
                 </label>
               </div>
-              <div>
-                <label>
-                  Booking Date:
+              <div className="pb-2">
+                <label className="text-xl">
+                  <span className="font-semibold">Booking Date: </span>
                   <input
                     type="date"
                     value={editingBooking.date}
@@ -167,9 +183,9 @@ const ProfilePage = () => {
                   />
                 </label>
               </div>
-              <div>
-                <label>
-                  Party Size:
+              <div className="pb-2">
+                <label className="text-xl">
+                  <span className="font-semibold">Party Size: </span>
                   <input
                     type="number"
                     value={editingBooking.partySize}
@@ -182,13 +198,52 @@ const ProfilePage = () => {
                   />
                 </label>
               </div>
-              <button type="submit">Save Changes</button>
-              <button type="button" onClick={() => setEditingBooking(null)}>
+              <div className="pb-2">
+                <label className="text-xl">
+                  <span className="font-semibold">Party Time: </span>
+                  <select
+                    value={editingBooking.partyTime}
+                    onChange={(e) =>
+                      setEditingBooking((prev) => ({
+                        ...prev,
+                        partyTime: e.target.value,
+                      }))
+                    }
+                  >
+                    <option value="Morning">Morning</option>
+                    <option value="Lunch">Lunch</option>
+                    <option value="Evening">Evening</option>
+                    <option value="Dinner">Dinner</option>
+                  </select>
+                </label>
+              </div>
+              <button
+                type="submit"
+                className="mt-2 border-2 border-red-700 text-red-700 hover:bg-red-700 hover:text-white mr-4 px-2 py-1 rounded-lg"
+              >
+                Save Changes
+              </button>
+              <button
+                type="button"
+                className="mt-2 border-2 border-red-700 text-red-700 hover:bg-red-700 hover:text-white mr-2 px-2 py-1 rounded-lg"
+                onClick={() => setEditingBooking(null)}
+              >
                 Cancel
               </button>
             </form>
           </div>
         )}
+
+        <div className="pt-4">
+          <form>
+            <button
+              onClick={handleLogout}
+              className="text-white hover:border-2 border-red-700 border-4 px-4 text-xl py-2 rounded-xl hover:border-gray-400 bg-red-700"
+            >
+              LogOut
+            </button>
+          </form>
+        </div>
       </div>
       <Footer />
     </div>
