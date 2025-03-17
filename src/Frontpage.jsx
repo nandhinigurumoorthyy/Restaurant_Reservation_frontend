723325f4bd5e4220331c8181edb29260bc995fc1
nandhinigurumoorthyy <nandhinigurumoorthy2003@gmail.com>
2025-03-17 19:47:04 +0530

    update

--- src/Frontpage.jsx
+++ src/Frontpage.jsx
@@ -6,7 +6,7 @@ import "./index.css";
 
 // Import your slider images
 import Image1 from "./images/image2.jpg";
-import Image2 from "./images/image1.jpg";
+import Image2 from "./images/image1.JPG";
 import Image3 from "./images/image3.jpg";
 import Image4 from "./images/image4.jpg";
 import Image5 from "./images/image5.jpg";
